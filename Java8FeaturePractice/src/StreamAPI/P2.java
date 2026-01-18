e250721a5af2c741baa4327fe9e8a8d4431f5c7c
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-01-18 19:32:57 +0530

    Stream API Map & Filter

--- Java8FeaturePractice/src/StreamAPI/P2.java
+++ Java8FeaturePractice/src/StreamAPI/P2.java
@@ -1,15 +1,18 @@
 package StreamAPI;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class P2 {
 	public static void main(String[] args) {
 	  ArrayList<Integer> num = new ArrayList<>();
-	  num.add(10);
-      num.add(20);
-	  num.add(30);
-	  num.add(40);
+	  num.add(1);
+      num.add(2);
+	  num.add(3);
+	  num.add(4);
 	  
 	  //Stream API[Sequential Stream]
 	  //Stream()
@@ -28,10 +31,27 @@ public class P2 {
 	  //Second time[IllegalStateException]
 	  //Snum.forEach(i -> System.out.println(i));
 	  
-			  
-	
-	
-	
+      // Using Filter()  
+	  // Predicate<Integer> p = i -> i%2==0;
+	  num.stream().filter(i -> i%2 == 0 /*or p*/ ).
+	  forEach(j -> System.out.println(j));
+	  
+	  // Using Map()  
+	  // Function<Integer, Integer> f = a -> a*a ;
+	  num.stream().filter(i -> i%2 == 0).
+	  map(/*f* or*/a-> a*a).forEach(j -> System.out.println(j));
+	  
+	  List<String> name = Arrays.asList("hitesh","harsh","vinod","mohit");
+	  
+	  // Print name Startwith h //
+	  Stream<String> n = name.stream();
+	  n.filter(i -> i.startsWith("h")).map(f -> f.toUpperCase()).
+	  forEach(e -> System.out.println(e));
+	  
+	  // Print name Not Startwith h //
+	  name.stream().filter(i ->! i.startsWith("h")).map(f -> f.toUpperCase()).
+	  forEach(e -> System.out.println(e));
+	  
 	}
 
 }
